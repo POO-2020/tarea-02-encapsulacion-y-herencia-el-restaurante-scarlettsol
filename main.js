a6262ba7fe03af3502b69f8fb400631277b26658
Marian Ramirez <mramirez83@ucol.mx>
2020-03-24 19:39:56 -0600

    Main

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import Restaurante from './resturante.js';
+import Restaurante from './restaurante.js';
 import Producto from './producto.js';
 import Precio from './precio.js';
 import ElementoPedido from './elemento-pedido.js';
@@ -7,37 +7,51 @@ import Fecha from './fecha.js';
 import Tiempo from './tiempo.js';
 import Cliente from './cliente.js';
 import Direccion from './direccion.js';
+import ClienteFrecuente from './cliente-frecuente.js';
 
 class Main {
   constructor() {
-    this._fecha = new Fecha(15, 6, 2005);
+  let pedido1 = {
+    numeroPedido: 1,
+    fecha: new Fecha(13, 3, 2020),
+    hora: new Tiempo(11, 17, 'AM'),
+    cliente: this._cliente2
+  }
 
-    this._tiempo1 = new Tiempo({ hora: 10, minuto: 25, periodo: 'am' });
-    this._tiempo2 = new Tiempo({ hora: 10, minuto: 25, periodo: 'pm' });
+  let pedido2 = {
+    numeroPedido: 2,
+    fecha: new Fecha(21, 3, 2020),
+    hora: new Tiempo(7, 22, 'PM'),
+    cliente: this._cliente3
+  }
 
-    this._direccion = new Direccion(
-      'Av. Universidad',
-      333,
-      0,
-      'Las Víboras',
-      28040,
-      'Colima',
-      'Colima'
-    );
+  this._pedido1 = new Pedido(pedido1);
+  this._pedido2 = new Pedido(pedido2);
 
-    this._precio = new Precio(2500.5);
+  this._fecha = new Fecha(15, 6, 2005);
 
-    this._producto = new Producto('Pizza Hawaiana', this._precio);
-    this._elemento = new ElementoPedido(2, this._producto);
-    this._elemento2 = new ElementoPedido(1, this._producto);
-    this._elemento3 = new ElementoPedido(3, this._producto);
+  this._tiempo1 = new Tiempo({ hora: 10, minuto: 25, periodo: 'am' });
+  this._tiempo2 = new Tiempo({ hora: 10, minuto: 25, periodo: 'pm' });
 
-    this._cliente = new Cliente('Juan Pérez Díaz', this._direccion, 3124563256);
+  this._direccion1 = new Direccion('Av. Universidad', 333, 0, 'Las Víboras', 28040, 'Colima', 'Colima');
+  this._direccion2 = new Direccion('Niños Héroes', 672, 'Lomas del Centenario', 28984, 'Colima', 'Villa de Alvarez');
+  this._direccion3 = new Direccion('Pablo Silva García', 249, 'Lomas del Centenario', 28984, 'Colima', 'Villa de Alvarez');
 
-    this._pedido = new Pedido(this._fecha, this._tiempo1, this._cliente);
-    this._pedido.agregarElemento(this._elemento);
-    this._pedido.agregarElemento(this._elemento2);
-    this._pedido.agregarElemento(this._elemento3);
+  this._precio = new Precio(2500.5);
+
+  this._producto = new Producto('Pizza Hawaiana', this._precio);
+  this._elemento = new ElementoPedido(2, this._producto);
+  this._elemento2 = new ElementoPedido(1, this._producto);
+  this._elemento3 = new ElementoPedido(3, this._producto);
+
+  this._cliente1 = new Cliente('Juan Pérez Díaz', this._direccion1, 3124563256);
+  this._cliente2 = new ClienteFrecuente('Javier Rodríguez', this._direccion2, 3126546891);
+  this._cliente3 = new Cliente('Andrea', this._direccion3, 3121534864);
+
+  this._pedido = new Pedido(this._fecha, this._tiempo1, this._cliente);
+  this._pedido.agregarElemento(this._elemento);
+  this._pedido.agregarElemento(this._elemento2);
+  this._pedido.agregarElemento(this._elemento3);
   }
 
   probarFecha() {
@@ -96,3 +110,4 @@ app.probarProducto();
 app.probarElementoPedido();
 app.probarCliente();
 app.probarPedido();
+app.probarClienteFrecuente();
